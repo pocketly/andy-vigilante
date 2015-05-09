beca4506c34c6e1b6a8508f34ceab2e05dfcb60b
Adam Jaso <ajaso@sazze.com>
2015-05-08 18:27:35 -0700

    added --camel2snake and --copy-only and --custom-sizes

--- index.js
+++ index.js
@@ -7,19 +7,31 @@ const Q = require('q');
 const fs = require('fs');
 const Path = require('path');
 const co = require('co');
+const _ = require('lodash');
 
 const SIZE_MAP = {
+  '': 'mdpi',
   '@1x': 'mdpi',
   '@1.5x': 'hdpi',
   '@2x': 'xhdpi',
   '@3x': 'xxhdpi'
 };
 
-const IMAGE_REGEX = /[^\/a-z_0-9]*([a-z_0-9]+?)[^\/a-z_0-9]*((?:xxxhdpi|xxhdpi|xhdpi|hdpi|mdpi|ldpi)|@\d(?:\.\d)?x)(\.png)$/;
+const IMAGE_REGEX = /[^\/a-z_0-9]*([A-Za-z_0-9]+?)[^\/a-z_0-9]*(xxxhdpi|xxhdpi|xhdpi|hdpi|mdpi|ldpi|@\d(?:\.\d)?x|)(\.png)$/;
 
 let args = require('minimist')(process.argv.slice(2));
 let src = ensurePrefix(args._[0] || './');
 let dest = ensurePrefix(args._[1] || './');
+let copyOnly = args['copy-only'];
+// camel2snake option here also
+
+let customSizeMap = args['custom-sizes'];
+if (customSizeMap) {
+  customSizeMap.split('|').forEach(function(size) {
+    let map = size.split('=');
+    SIZE_MAP[map[0]] = map[1];
+  });
+}
 
 if (args['relative-dest']) {
   dest = mkpath(src, args['relative-dest']);
@@ -27,7 +39,7 @@ if (args['relative-dest']) {
 
 log('started watching', src, '->', dest);
 
-chk.watch(src, {ignored: /([\/\\]\.|\.idea|node_modules)/}).on('all', co.wrap(function *(event, path) {
+let watcher = chk.watch(src, {ignored: /([\/\\]\.|\.idea|node_modules)/}).on('all', co.wrap(function *(event, path) {
 
   try {
 
@@ -39,8 +51,9 @@ chk.watch(src, {ignored: /([\/\\]\.|\.idea|node_modules)/}).on('all', co.wrap(fu
     if ('unlink' != event && (matches = path.match(IMAGE_REGEX))) {
 
 
-      let filename = matches[1] + matches[3];
+      let filename = (args.camel2snake ? camel2snake(matches[1]) : matches[1]) + matches[3];
       let fileType = matches[2];
+      //console.log(args.camel2snake, camel2snake(matches[1]), matches[1], filename, fileType, 'ddd');
 
       if (process.env.NODE_DEBUG) {
         log(matches);
@@ -50,9 +63,10 @@ chk.watch(src, {ignored: /([\/\\]\.|\.idea|node_modules)/}).on('all', co.wrap(fu
 
       let destPath = yield getDrawablePath(fileType, filename);
 
-      log('moving', srcPath, '->', destPath);
+      log((copyOnly ? 'copying' : 'moving'), srcPath, '->', destPath);
 
       yield move(srcPath, destPath);
+      log(copyOnly ? 'copied' : 'moved');
     }
 
   } catch (e) {
@@ -62,6 +76,9 @@ chk.watch(src, {ignored: /([\/\\]\.|\.idea|node_modules)/}).on('all', co.wrap(fu
 
 }));
 
+fs.readdirSync(src).forEach(function(val) {
+  watcher.emit('all', 'add', mkpath(src, val));
+});
 
 
 // util functions
@@ -69,12 +86,17 @@ chk.watch(src, {ignored: /([\/\\]\.|\.idea|node_modules)/}).on('all', co.wrap(fu
 function move(srcPath, destPath) {
   return co(function *() {
     try {
-      return yield Q.nfcall(fs.rename, srcPath, destPath);
+      return yield Q.nfcall(copyOnly ? copyFile : fs.rename, srcPath, destPath);
     } catch (e) {
       log(e);
       return false;
     }
   });
+}
+
+function copyFile(src, dest, cb) {
+  cb = _.once(cb);
+  fs.createReadStream(src).pipe(fs.createWriteStream(dest)).on('close', cb).on('error', cb);
 }
 
 function getSourcePath(filename) {
@@ -83,7 +105,7 @@ function getSourcePath(filename) {
 
 function getDrawablePath(type, filename) {
   return co(function *() {
-    if (!type) {
+    if ('string' !== typeof type) {
       return null;
     }
 
@@ -143,6 +165,10 @@ function ensurePrefix(dir) {
 
 function mkpath(p1, p2) {
   return p1.replace(/\/$/, '') + '/' + p2.replace(/^\//, '');
+}
+
+function camel2snake(camelCase) {
+  return camelCase.replace(/[A-Z]/g, function(match) {return '_' + match.toLowerCase();})
 }
 
 function log() {
